b6e84ed5d8235e902e119c5eecc5310a410bb5f3
PyConqueror <wanaqim@gmail.com>
2025-10-24 18:48:17 +0800

    feat(layout) : import navbar and footer in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
+import Navbar from "@/components/navbar"
+import Footer from "@/components/footer"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -24,7 +26,9 @@ export default function RootLayout({
       </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+          <Navbar />
           {children}
+          <Footer />
         </ThemeProvider>
       </body>
     </html>
