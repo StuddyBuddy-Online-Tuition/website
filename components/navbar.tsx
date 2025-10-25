d72282e15b54adc83760a9e6f8c5eff6305ca1bd
PyConqueror <wanaqim@gmail.com>
2025-10-25 14:41:26 +0800

    hotfix : fix navbar href link

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -14,22 +14,22 @@ export default function Navbar() {
       <div className="container flex h-16 items-center justify-between px-4 md:px-6">
         <Link href="/" className="flex items-center gap-2">
           <Image src="/logo.png" alt="Study Buddy Logo" width={50} height={50} className="rounded-full" />
-          <span className="text-xl font-bold text-[#0e2e47]">Study Buddy</span>
+          <span className="text-xl font-bold text-[#0e2e47]">StudyBuddy Tutors</span>
         </Link>
         <nav className="hidden md:flex gap-6">
-          <Link href="#about" className="text-[#0e2e47] hover:text-[#00a8e8] font-medium transition-colors">
+          <Link href="/#about" className="text-[#0e2e47] hover:text-[#00a8e8] font-medium transition-colors">
             About
           </Link>
-          <Link href="#why-us" className="text-[#0e2e47] hover:text-[#00a8e8] font-medium transition-colors">
+          <Link href="/#why-us" className="text-[#0e2e47] hover:text-[#00a8e8] font-medium transition-colors">
             Why Us
           </Link>
-          <Link href="#teachers" className="text-[#0e2e47] hover:text-[#00a8e8] font-medium transition-colors">
+          <Link href="/#teachers" className="text-[#0e2e47] hover:text-[#00a8e8] font-medium transition-colors">
             Teachers
           </Link>
-          <Link href="#subjects" className="text-[#0e2e47] hover:text-[#00a8e8] font-medium transition-colors">
+          <Link href="/#subjects" className="text-[#0e2e47] hover:text-[#00a8e8] font-medium transition-colors">
             Subjects
           </Link>
-          <Link href="#contact" className="text-[#0e2e47] hover:text-[#00a8e8] font-medium transition-colors">
+          <Link href="/#contact" className="text-[#0e2e47] hover:text-[#00a8e8] font-medium transition-colors">
             Contact
           </Link>
         </nav>
@@ -46,35 +46,35 @@ export default function Navbar() {
         <div className="md:hidden bg-white p-4 border-t">
           <nav className="flex flex-col gap-4">
             <Link
-              href="#about"
+              href="/#about"
               className="text-[#0e2e47] hover:text-[#00a8e8] font-medium transition-colors"
               onClick={() => setIsMenuOpen(false)}
             >
               About
             </Link>
             <Link
-              href="#why-us"
+              href="/#why-us"
               className="text-[#0e2e47] hover:text-[#00a8e8] font-medium transition-colors"
               onClick={() => setIsMenuOpen(false)}
             >
               Why Us
             </Link>
             <Link
-              href="#teachers"
+              href="/#teachers"
               className="text-[#0e2e47] hover:text-[#00a8e8] font-medium transition-colors"
               onClick={() => setIsMenuOpen(false)}
             >
               Teachers
             </Link>
             <Link
-              href="#subjects"
+              href="/#subjects"
               className="text-[#0e2e47] hover:text-[#00a8e8] font-medium transition-colors"
               onClick={() => setIsMenuOpen(false)}
             >
               Subjects
             </Link>
             <Link
-              href="#contact"
+              href="/#contact"
               className="text-[#0e2e47] hover:text-[#00a8e8] font-medium transition-colors"
               onClick={() => setIsMenuOpen(false)}
             >
